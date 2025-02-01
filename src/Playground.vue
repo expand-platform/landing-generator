064f49713e768ce3c4c9741687b80ae4ec11a37b
Danya <ovrashkod@gmail.com>
2025-02-01 10:41:20 +0200

    тестил слайдеры

--- src/Playground.vue
+++ src/Playground.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import Header from '@sections/Header.vue'
+import Slider from '@sections/Slider.vue'
+
 </script>
 
 <template>
-  <h2 class="title text-center my-3"> PlayGround </h2>
+  <!-- <h2 class="title text-center my-3"> PlayGround </h2> -->
   <Header></Header>
+  <div class="slider">
+    <Slider></Slider>
+  </div>
+
   <!-- <Header logo="image" :menu="[]" color="#000"></Header> -->
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  // .slider {
+  //   padding: 400px;
+  //   padding-top: 200px;
+  // }
+</style>
---
+++ src/components/sections/Slider.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import { CCarousel, CCarouselItem } from "@coreui/bootstrap-vue"
+</script>
+
+<template>
+    <CCarousel controls>
+  <CCarouselItem>
+    <img class="d-block w-100" src="https://dummyimage.com/500x300.png?text=Hello%20World" alt="slide 1"/>
+  </CCarouselItem>
+  <CCarouselItem>
+    <img class="d-block w-100" src="https://dummyimage.com/500x300.png?text=Hello%20World" alt="slide 2"/>
+  </CCarouselItem>
+  <CCarouselItem>
+    <img class="d-block w-100" src="https://dummyimage.com/500x300.png?text=Hello%20World" alt="slide 3"/>
+  </CCarouselItem>
+</CCarousel>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>

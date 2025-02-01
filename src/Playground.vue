f722ceb6635781a758154f6d87f40befbcfa78e1
nazar-sulima <nazarukrainian47@gmail.com>
2025-02-01 12:07:14 +0000

    Create Features component and config folder

--- src/Playground.vue
+++ src/Playground.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import Header from '@sections/Header.vue'
+import Features from './components/sections/Features.vue';
+import { features } from './config/features';
+
+
 </script>
 
 <template>
   <h2 class="title text-center my-3"> PlayGround </h2>
   <Header logo-type="text"></Header>
   <!-- <Header logo="image" :menu="[]" color="#000"></Header> -->
+  <Features card-text="This is text for card" :cards="features"></Features>
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/sections/Features.vue
@@ -0,0 +1,49 @@
+  <script setup lang="ts">
+import {
+  CCard,
+  CCardImage,
+  CCardBody,
+  CCardTitle,
+  CCardText,
+  CButton,
+  CContainer,
+  CRow,
+  CCol,
+} from '@coreui/bootstrap-vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+console.log(count.value)
+let image = 'https://dummyimage.com/100.png'
+
+interface Card {
+  text: string
+  image: string
+}
+
+let props = defineProps<{
+  cardText?: string
+  cards?: Card[]
+}>()
+
+console.log(props.cardText)
+</script>
+
+<template>
+  <CContainer>
+    <CRow>
+      <CCol v-for="card in cards">
+        <CCard style="width: 18rem" >
+          <CCardBody>
+            <CCardTitle>{{ card.text }}</CCardTitle>
+            <CCardText>{{ cardText }}</CCardText>
+            <CButton href="#" color="primary">Go somewhere</CButton>
+          </CCardBody>
+          <CCardImage orientation="bottom" :src="card.image" alt="" />
+        </CCard>
+      </CCol>
+    </CRow>
+  </CContainer>
+</template>
+
+<style lang="scss" scoped></style>

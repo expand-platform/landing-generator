55b2a520540e5ea749298aa4d4b00fd6ec9c26c3
Danya <ovrashkod@gmail.com>
2025-02-22 11:48:58 +0200

    more slider responsiveness

--- src/components/sections/Slider.vue
+++ src/components/sections/Slider.vue
@@ -17,7 +17,7 @@ let width = window.innerWidth
     :fade="configs.fade" :interval="configs.interval">
     <BCarouselSlide v-for="image in configs.images" :key="image.src" class="carousel-img" :img-src="image.src">
       <template #caption>
-        <h2>
+        <h2 class="caption">
           {{ image.caption }}
         </h2>
       </template>
@@ -25,10 +25,10 @@ let width = window.innerWidth
   </BCarousel>
 
   <!-- mobile -->
-  <BCarousel v-else :style="configs.style" :fade="configs.fade" :interval="configs.interval" :ride="configs.ride">
+  <BCarousel v-else :style="configs.style" :fade="configs.fade" :interval="configs.interval" :ride="configs.ride as any">
     <BCarouselSlide v-for="image in configs.images" :key="image.src" class="carousel-img" :img-src="image.src">
       <template #caption>
-        <h2>
+        <h2 class="caption">
           {{ image.caption }}
         </h2>
       </template>
@@ -38,9 +38,38 @@ let width = window.innerWidth
 </template>
 
 <style lang="scss" scoped>
+@use "@scss/base/media.scss";
+
 .carousel-img {
   max-width: 100vw;
   max-height: 100vh;
-
 }
+
+.carousel-caption {
+  padding-bottom: 0.75rem !important;
+  bottom: 0.75rem !important;
+}
+
+.caption {
+  text-shadow: 
+    -1px -1px 0 black,  
+    1px -1px 0 black,  
+    -1px  1px 0 black,  
+    1px  1px 0 black;
+     
+  margin-bottom: -100px;
+  font-size: 20px;
+  
+  @include media.sm {
+    font-size: 40px;
+    margin-bottom: -15px;
+  }
+  @include media.md {
+    font-size: 60px;
+  }
+  @include media.lg {
+    margin-bottom: 0;
+  }
+}
+
 </style>
